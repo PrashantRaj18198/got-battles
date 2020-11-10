12501460f457c8eba53256411aa6048b65df394d
prashantraj18198 <prashantraj18198@gmail.com>
2020-11-10 23:50:43 +0530

    static serve react build added

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,11 @@ const battleRouter = require('./routes/battleRoutes');
 
 const app = express();
 app.use(express.json());
+app.use(express.static(`${__dirname}/build`));
 app.use(cors());
 
 const { username, password, dbname } = settings['db'];
-const port = settings['port'];
+const port = process.env.PORT || settings['port'];
 const connectionMethod = settings['connectionMethod'];
 let uri = '';
 
